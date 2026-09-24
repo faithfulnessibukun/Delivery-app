5953b362cd51197581f8e3f63e3b51140852f7ef
ibukun <faithfulnessibukun@gmail.com>
2026-09-24 17:20:02 +0100

    jjj

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Replace these two strings with your actual values from:
+// Replace these two strings with your actual Supabase credentials from:
 // Supabase Dashboard -> Project Settings -> API
 const supabaseUrl = "https://vmnxjhgojuabccuwljpk.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtbnhqaGdvanVhYmNjdXdsanBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNzc0NzksImV4cCI6MjEwMzc1MzQ3OX0.K8HR1BFkqAYJmSNTtv7l5vaA_vSh82trw4wqlBWRLuM";

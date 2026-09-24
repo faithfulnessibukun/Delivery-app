7b3bd38942ca2e1a9c16de5450b245dae716ec1a
ibukun <faithfulnessibukun@gmail.com>
2026-09-24 17:07:10 +0100

    ggf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  build: {
+    // Set the chunk size limit in kilobytes (kB). 
+    // Default is 500. Setting it to 1000 raises the warning threshold to 1 MB.
+    chunkSizeWarningLimit: 1000,
+  },
 });
